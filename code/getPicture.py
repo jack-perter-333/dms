1c181b89127da47038b58c3b4f5f58d24eba310b
jack-perter-333 <51489785+jack-perter-333@users.noreply.github.com>
2021-01-25 17:38:43 +0700

    Update getPicture.py

--- code/getPicture.py
+++ code/getPicture.py
@@ -7,12 +7,18 @@ def clearHTML(path):
    r= re.search('''<header.*>[\s\S\d\w\W]*<div xmlns="" id="idm92303861" style="display: none;" class="table-wrapper ">''',fHTML)
    #loai bo floot
    r1 = re.search('''<div xmlns="" style="height: 40.*[\s\S\d\w\W]*</div>''',fHTML)
-
+   r2 = re.search('''<div class="clear"></div>
+</div>
+<span[\s\S\d\w\W]*<div xmlns="" id="idm92303861" style="display: none;" class="table-wrapper ">''',fHTML)
+   
    if r:
       replace=r.group()
       fHTML=fHTML.replace(replace,"")
    if r1:
       replace=r1.group()
+      fHTML=fHTML.replace(replace,"")
+   if r2 :
+      replace=r2.group()
       fHTML=fHTML.replace(replace,"")
    t = '''<span onclick="toggle('idm92303861');" class="button" style="margin-top: 20px">Hide</span><div class="clear"></div>'''
    fHTML = fHTML.replace(t,'')
